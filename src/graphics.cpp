90f4f6c495e0b4133b02c41e9f4ea8b2bcdd76f3
tinaxd <31384595+tinaxd@users.noreply.github.com>
2021-02-24 10:57:38 +0900

    basic movement

--- src/graphics.cpp
+++ src/graphics.cpp
@@ -7,9 +7,35 @@
 
 using namespace mycraft;
 
+Renderer *mycraft::global_renderer = nullptr;
+
+class mycraft::KeyboardHandlerData
+{
+public:
+	KeyboardHandlerData() {}
+
+private:
+	enum class KeyboardButton {
+		KEY_W,
+		KEY_S,
+		KEY_A,
+		KEY_D,
+		KEY_SHIFT,
+		KEY_SPACE
+	};
+
+	std::array<bool, 6> button_state_;
+
+	friend void mycraft::keyboard_handler(GLFWwindow *window, int key, int scancode, int action, int mods);
+	friend void mycraft::keyboard_handler_tick();
+};
+
 Renderer::Renderer(int window_width, int window_height,
 		const std::string &window_title)
+	: keyboard_handler_data_(new KeyboardHandlerData)
 {
+	global_renderer = this;
+
 	glfwInit();
 
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -26,6 +52,16 @@ Renderer::Renderer(int window_width, int window_height,
 	glewInit();
 
 	prepare_shaders();
+
+	// TODO: when to set callback?
+	glfwSetKeyCallback(window_, &keyboard_handler);
+
+	// mouse motion callback
+	glfwSetInputMode(window_, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	if (glfwRawMouseMotionSupported())
+		glfwSetInputMode(window_, GLFW_RAW_MOUSE_MOTION, GLFW_TRUE);
+	glfwSetCursorPosCallback(window_, &mousemotion_handler);
+	glfwGetCursorPos(window_, &last_cursor_xpos, &last_cursor_ypos);
 }
 
 struct ShaderCompileError: std::runtime_error
@@ -131,13 +167,14 @@ void Renderer::Renderer::render_loop()
 	GLint pos_attrib = glGetAttribLocation(shader_program_, "block");
 	assert(pos_attrib >= 0);
 	glEnableVertexAttribArray(pos_attrib);
-	glVertexAttribPointer(pos_attrib, 3, GL_BYTE, GL_FALSE, 5*sizeof(GLbyte), 0);
+	glVertexAttribPointer(pos_attrib, 3, GL_BYTE, GL_FALSE, 5 * sizeof(GLbyte),
+			0);
 
 	GLint texcoord_attrib = glGetAttribLocation(shader_program_, "texCoord");
 	assert(texcoord_attrib >= 0);
 	glEnableVertexAttribArray(texcoord_attrib);
-	glVertexAttribPointer(texcoord_attrib, 2, GL_BYTE, GL_FALSE, 5*sizeof(GLbyte),
-			(void*)(3*sizeof(GLbyte)));
+	glVertexAttribPointer(texcoord_attrib, 2, GL_BYTE, GL_FALSE,
+			5 * sizeof(GLbyte), (void*) (3 * sizeof(GLbyte)));
 
 	// setup uniforms
 	model_uni_ = glGetUniformLocation(shader_program_, "model");
@@ -154,22 +191,31 @@ void Renderer::Renderer::render_loop()
 			1.0, 50.0);
 	glUniformMatrix4fv(proj_uni_, 1, GL_FALSE, glm::value_ptr(proj_mat));
 
+	// view initial position
+	// TODO: set meaningful initial position
+	view_pos_ = glm::vec3(0.0, 0.0, 20);
+
 	// set winding order to Clockwise
 	glFrontFace(GL_CW);
 
 	// load textures
 	load_textures();
 
+	last_update_time = std::chrono::high_resolution_clock::now();
 	while (!glfwWindowShouldClose(window_))
 	{
 		glfwSwapBuffers(window_);
 		glfwPollEvents();
 
+		// player input
+		keyboard_handler_tick();
+
 		// draw
 		glClearColor(0, 0, 0, 1);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		//render_world(0, 0, 0); // TODO: input camera's coord
-		render_world(-10, -15, 20);
+		render_world();
+
+		last_update_time = std::chrono::high_resolution_clock::now();
 	}
 }
 
@@ -178,16 +224,14 @@ Renderer::~Renderer()
 	glfwTerminate();
 }
 
-void Renderer::render_world(CoordElem pos_x, CoordElem pos_y, CoordElem pos_z)
+void Renderer::render_world()
 {
 	if (!world_ || !ts_)
 		return;
 
 	// view
-	glm::mat4 view_mat = glm::lookAt(glm::vec3(pos_x, pos_y, pos_z),
-			glm::vec3(0, 0, 20), glm::vec3(0, 0, 1));
-	//glm::mat4 view_mat(1.0f);
-	glUniformMatrix4fv(view_uni_, 1, GL_FALSE, glm::value_ptr(view_mat));
+	glm::mat4 view_pos_mat = glm::lookAt(view_pos_, view_pos_+view_look_at_vec_, glm::vec3(0, 0, 1));
+	glUniformMatrix4fv(view_uni_, 1, GL_FALSE, glm::value_ptr(view_pos_mat));
 
 	for (int i = -1; i <= 1; i++)
 	{
@@ -230,8 +274,10 @@ void Renderer::load_textures()
 	int width, height;
 	glActiveTexture(GL_TEXTURE0);
 	glBindTexture(GL_TEXTURE_2D, texture);
-	unsigned char *image = SOIL_load_image("resources/texture.png", &width, &height, 0, SOIL_LOAD_RGB);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+	unsigned char *image = SOIL_load_image("resources/texture.png", &width,
+			&height, 0, SOIL_LOAD_RGB);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
+			GL_UNSIGNED_BYTE, image);
 	SOIL_free_image_data(image);
 
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
@@ -247,12 +293,7 @@ void Renderer::load_textures()
 
 enum TexDir
 {
-	TEX_XNEG,
-	TEX_YNEG,
-	TEX_XPOS,
-	TEX_YPOS,
-	TEX_ZNEG,
-	TEX_ZPOS
+	TEX_XNEG, TEX_YNEG, TEX_XPOS, TEX_YPOS, TEX_ZNEG, TEX_ZPOS
 };
 
 size_t Renderer::load_chunk_vertices(const Chunk &chunk)
@@ -265,41 +306,41 @@ size_t Renderer::load_chunk_vertices(const Chunk &chunk)
 
 	GLbyte gx, gy, gz;
 	auto s =
-			[&a, &vertices, &chunk, &gx, &gy, &gz, ts_=this->ts_](GLbyte x, GLbyte y,
-					GLbyte z, TexDir tex_dir, bool tex_x, bool tex_y)
+			[&a, &vertices, &chunk, &gx, &gy, &gz, ts_=this->ts_](GLbyte x,
+					GLbyte y, GLbyte z, TexDir tex_dir, bool tex_x, bool tex_y)
 					{
 						auto& array = vertices[a++];
 						array[0] = x; // pos x
-						array[1] = y; // pos y
-						array[2] = z; // pos z
-						const auto blk_id = chunk.data()[Chunk::convert_index(gx, gy, gz)].block_id();
-						// TODO: texture id
-						const auto& tex_ = ts_->texture(blk_id-1);
-						TextureMapCoord tex;
-						switch (tex_dir)
-						{
+					array[1] = y;// pos y
+					array[2] = z;// pos z
+					const auto blk_id = chunk.data()[Chunk::convert_index(gx, gy, gz)].block_id();
+					// TODO: texture id
+					const auto& tex_ = ts_->texture(blk_id-1);
+					TextureMapCoord tex;
+					switch (tex_dir)
+					{
 						case TEX_XNEG:
-							tex = tex_.xneg();
-							break;
+						tex = tex_.xneg();
+						break;
 						case TEX_YNEG:
-							tex = tex_.yneg();
-							break;
+						tex = tex_.yneg();
+						break;
 						case TEX_XPOS:
-							tex = tex_.xpos();
-							break;
+						tex = tex_.xpos();
+						break;
 						case TEX_YPOS:
-							tex = tex_.ypos();
-							break;
+						tex = tex_.ypos();
+						break;
 						case TEX_ZNEG:
-							tex = tex_.zneg();
-							break;
+						tex = tex_.zneg();
+						break;
 						case TEX_ZPOS:
-							tex = tex_.zpos();
-							break;
-						}
-						array[3] = !tex_x ? tex.first.first : tex.second.first; // tex x
-						array[4] = !tex_y ? tex.first.second : tex.second.second; // tex y
-					};
+						tex = tex_.zpos();
+						break;
+					}
+					array[3] = !tex_x ? tex.first.first : tex.second.first; // tex x
+					array[4] = !tex_y ? tex.first.second : tex.second.second;// tex y
+				};
 
 	auto exists = [&chunk](GLbyte x, GLbyte y, GLbyte z)
 	{
@@ -316,7 +357,7 @@ size_t Renderer::load_chunk_vertices(const Chunk &chunk)
 
 			for (int k = 0; k < Chunk::chunk_height; k++)
 			{
-				const auto& blk = chunk.data()[Chunk::convert_index(i, j, k)];
+				const auto &blk = chunk.data()[Chunk::convert_index(i, j, k)];
 
 				if (blk.block_id() == 0)
 				{
@@ -339,7 +380,7 @@ size_t Renderer::load_chunk_vertices(const Chunk &chunk)
 //					vertices[drawn.at(0)+3][2] = k+1;
 //				}
 //				else
-					if (j == 0 || !exists(i, j - 1, k))
+				if (j == 0 || !exists(i, j - 1, k))
 				{
 					//drawn.at(0) = a;
 					s(i, j, k, TEX_YNEG, false, false);
@@ -358,7 +399,7 @@ size_t Renderer::load_chunk_vertices(const Chunk &chunk)
 //					vertices[drawn.at(1)+3][2] = k+1;
 //				}
 //				else
-					if (i == Chunk::chunk_length - 1 || !exists(i + 1, j, k))
+				if (i == Chunk::chunk_length - 1 || !exists(i + 1, j, k))
 				{
 					//drawn.at(1) = a;
 					s(i + 1, j, k, TEX_XPOS, false, false);
@@ -377,7 +418,7 @@ size_t Renderer::load_chunk_vertices(const Chunk &chunk)
 //					vertices[drawn.at(2)+3][2] = k+1;
 //				}
 //				else
-					if (j == Chunk::chunk_length - 1 || !exists(i, j + 1, k))
+				if (j == Chunk::chunk_length - 1 || !exists(i, j + 1, k))
 				{
 					//drawn.at(2) = a;
 					s(i + 1, j + 1, k, TEX_YPOS, false, false);
@@ -396,7 +437,7 @@ size_t Renderer::load_chunk_vertices(const Chunk &chunk)
 //					vertices[drawn.at(3)+3][2] = k+1;
 //				}
 //				else
-					if (i == 0 || !exists(i - 1, j, k))
+				if (i == 0 || !exists(i - 1, j, k))
 				{
 					//drawn.at(3) = a;
 					s(i, j + 1, k, TEX_XNEG, false, false);
@@ -411,7 +452,7 @@ size_t Renderer::load_chunk_vertices(const Chunk &chunk)
 //				if (can_merge)
 //				{}
 //				else
-					if (k == 0 || !exists(i, j, k - 1))
+				if (k == 0 || !exists(i, j, k - 1))
 				{
 					//drawn.at(4) = a;
 					s(i, j, k, TEX_ZNEG, false, false);
@@ -429,7 +470,7 @@ size_t Renderer::load_chunk_vertices(const Chunk &chunk)
 //						vertices[drawn.at(5)+i][2] = k+1;
 //				}
 //				else
-					if (k == Chunk::chunk_height - 1 || !exists(i, j, k + 1))
+				if (k == Chunk::chunk_height - 1 || !exists(i, j, k + 1))
 				{
 					//drawn.at(5) = a;
 					s(i, j, k + 1, TEX_ZPOS, false, false);
@@ -444,8 +485,115 @@ size_t Renderer::load_chunk_vertices(const Chunk &chunk)
 	}
 
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(GL_ARRAY_BUFFER, elem_count * a * sizeof(GLbyte), vertices.data(),
+	glBufferData(GL_ARRAY_BUFFER, elem_count * a * sizeof(GLbyte),
+			vertices.data(),
 			GL_STATIC_DRAW);
 	chunk.set_changed(false);
 	return elem_count * a;
+}
+
+void mycraft::keyboard_handler(GLFWwindow *window, int key, int scancode,
+		int action, int mods)
+{
+	Renderer *self = global_renderer;
+	auto& data = self->keyboard_handler_data_;
+
+	auto& state = data->button_state_;
+
+	auto ed = [&state, &action, &key](int key_target, KeyboardHandlerData::KeyboardButton btn) {
+		if (key == key_target) {
+			if (action == GLFW_PRESS)
+				state[static_cast<size_t>(btn)] = true;
+			else if (action == GLFW_RELEASE)
+				state[static_cast<size_t>(btn)] = false;
+		}
+	};
+
+	using KB = KeyboardHandlerData::KeyboardButton;
+	ed(GLFW_KEY_W, KB::KEY_W);
+	ed(GLFW_KEY_S, KB::KEY_S);
+	ed(GLFW_KEY_A, KB::KEY_A);
+	ed(GLFW_KEY_D, KB::KEY_D);
+	ed(GLFW_KEY_SPACE, KB::KEY_SPACE);
+	ed(GLFW_KEY_LEFT_SHIFT, KB::KEY_SHIFT);
+	// TODO: GLFW_KEY_RIGHT_SHIFT?
+}
+
+void mycraft::keyboard_handler_tick()
+{
+	Renderer *self = global_renderer;
+	const auto& lookat_vec = self->view_look_at_vec_;
+	const auto& speed = self->walk_speed;
+	const auto& state = self->keyboard_handler_data_->button_state_;
+	using KB = KeyboardHandlerData::KeyboardButton;
+	auto& view_pos = self->view_pos_;
+	float dt = std::chrono::duration_cast<std::chrono::duration<float>>(
+			std::chrono::high_resolution_clock::now() - self->last_update_time).count();
+
+	const glm::vec2 forward = glm::normalize(glm::vec2(lookat_vec.x, lookat_vec.y)) * speed * dt;
+	glm::vec3 vel(0.0, 0.0, 0.0);
+	if (state[static_cast<size_t>(KB::KEY_W)])
+	{
+		vel.x += forward.x;
+		vel.y += forward.y;
+	}
+	if (state[static_cast<size_t>(KB::KEY_S)])
+	{
+		vel.x -= forward.x;
+		vel.y -= forward.y;
+	}
+	if (state[static_cast<size_t>(KB::KEY_A)])
+	{
+		vel.x -= forward.y;
+		vel.y += forward.x;
+	}
+	if (state[static_cast<size_t>(KB::KEY_D)])
+	{
+		vel.x += forward.y;
+		vel.y -= forward.x;
+	}
+	if (state[static_cast<size_t>(KB::KEY_SPACE)])
+	{
+		vel.z += speed*dt;
+	}
+	if (state[static_cast<size_t>(KB::KEY_SHIFT)])
+	{
+		vel.z -= speed*dt;
+	}
+
+	view_pos += vel;
+}
+
+void mycraft::mousemotion_handler(GLFWwindow *window, double xpos, double ypos)
+{
+	Renderer *self = global_renderer;
+
+	double dx = xpos - self->last_cursor_xpos;
+	double dy = ypos - self->last_cursor_ypos;
+	self->last_cursor_xpos = xpos;
+	self->last_cursor_ypos = ypos;
+
+	// TODO: user settings
+	float dx_speed = -0.0005f;
+	float dy_speed = -0.0005f;
+
+	auto& ax = self->camera_angles_x;
+	auto& ay = self->camera_angles_y;
+
+	ax += dx * dx_speed;
+	if (ax < -M_PI)
+		ax += 2*M_PI;
+	else if (ax > M_PI)
+		ax -= 2*M_PI;
+
+	ay += dy * dy_speed;
+	if (ay < -M_PI/2)
+		ay = -M_PI/2;
+	else if (ay > M_PI/2)
+		ay = M_PI/2;
+
+	auto& lookat = self->view_look_at_vec_;
+	lookat.x = std::cos(ax) * std::cos(ay);
+	lookat.y = std::sin(ax) * std::cos(ay);
+	lookat.z = std::sin(ay);
 }
--- src/graphics.h
+++ src/graphics.h
@@ -12,9 +12,16 @@
 #include <memory>
 #include "world.h"
 #include "TextureMap.h"
+#include <chrono>
 
 namespace mycraft
 {
+	class KeyboardHandlerData;
+	void keyboard_handler(GLFWwindow *window, int key, int scancode, int action, int mods);
+	void keyboard_handler_tick();
+	void mousemotion_handler(GLFWwindow *window, double xpos, double ypos);
+
+	// TODO: this class should be a singleton.
 	class Renderer
 	{
 	public:
@@ -47,20 +54,47 @@ namespace mycraft
 
 	private:
 		GLFWwindow *window_;
+		// World data
 		std::shared_ptr<World> world_;
+
+		// Texture data
 		std::shared_ptr<TextureStorage> ts_;
+
+		// OpenGL
 		GLuint shader_program_;
 		GLuint vbo;
 
+		float walk_speed = 100.0;
+		// player's position
+		glm::vec3 view_pos_;
+		glm::vec3 view_look_at_vec_;
 		GLint view_uni_;
+
 		GLint proj_uni_;
 
 		GLint model_uni_;
 
+		// time
+		std::chrono::high_resolution_clock::time_point last_update_time;
+
+		// keyboard input
+		std::unique_ptr<KeyboardHandlerData> keyboard_handler_data_;
+
+		// mouse input
+		double last_cursor_xpos, last_cursor_ypos;
+		float camera_angles_x = .0f;
+		float camera_angles_y = .0f;
+
 		void prepare_shaders();
 		void load_textures();
-		void render_world(CoordElem pos_x, CoordElem pos_y, CoordElem pos_z);
+		void render_world();
 
 		size_t load_chunk_vertices(const Chunk& chunk);
+
+		friend void keyboard_handler(GLFWwindow *window, int key, int scancode, int action, int mods);
+		friend void mousemotion_handler(GLFWwindow *window, double xpos, double ypos);
+		friend void keyboard_handler_tick();
 	};
+
+	extern Renderer *global_renderer;
 }
